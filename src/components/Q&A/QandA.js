c6423cb17a0673758acf9fc0e8bd8b02e436c0c9
christoffer malmgren <malmgrenchristoffer@hotmail.com>
2021-08-18 14:38:00 +0200

    qanda finished

--- src/components/Q&A/QandA.js
+++ src/components/Q&A/QandA.js
@@ -4,20 +4,42 @@ import classes from './QandA.module.css'
 export const QandA = () => {
     return (
         <div id='qAndA' className={classes.wrapperQandA}>
+          <div className={classes.qanda}>Q&A</div>
           <div className={classes.qAndAText}>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus quis
-            numquam reiciendis quaerat dolorum neque in vitae voluptatem, rem
-            asperiores, sed eius quod praesentium vero quasi repellendus odio
-            pariatur nam impedit fuga ad dicta optio. Ab adipisci aliquam numquam
-            hic porro, et sed qui tenetur soluta beatae fugit explicabo. Amet
-            ducimus harum tenetur repellat temporibus fugiat soluta earum. Nesciunt
-            dolorem ex quas ad in dolorum iusto nemo repellat voluptas aperiam,
-            culpa repudiandae ipsa cupiditate similique molestias tenetur nostrum.
-            Quod numquam exercitationem minus itaque molestias expedita
-            necessitatibus debitis eveniet nostrum. Magni quos voluptate commodi.
-            Alias sunt officia impedit dolorem qui laborum fugiat odit eveniet
-            itaque quas nemo doloremque sint, quaerat aspernatur dicta ad! Porro
-            nisi tenetur enim ipsa? Id, quis numquam?
+            <span className={classes.paragraph}>
+            Is it for both boys and female player?
+            </span>
+            Yes of course, we work with both genders who wants and need our help.
+          </div>
+          <div className={classes.qAndAText}>
+            <span className={classes.paragraph}>Do I have to pay, and when?</span>
+            No, not in the beginning. First of all you sign up to our database so we know that you exist. You only pay when the transfer or new contract is done.
+          </div>
+          <div className={classes.qAndAText}>
+            <span className={classes.paragraph}>I have a contract. Can i sign up anyway?</span>
+            Of course, no problem. If you have a contract today, then that is yours to honor. But we treat your case confidentially so we keep your profile during your time with us private.
+If a club is interested in you, we contact you and take the it from there if or when that happens, with full descretion.
+
+          </div>
+          <div className={classes.qAndAText}>
+            <span className={classes.paragraph}>How “good” do i have to be too sign up?</span>
+            If you play in one of the two higest leagues you are interseted for the bank and also we have the new concept JACT prospect sessions that let us take in more players to look closer into. If you have any questions - just email us.
+          </div>
+          <div className={classes.qAndAText}>
+            <span className={classes.paragraph}>I am a coach, can i sign up too?</span>
+            Absolutely, many clubs and unions are always looking for alternatives and after coaches or other competencies around a team.
+          </div>
+          <div className={classes.qAndAText}>
+            <span className={classes.paragraph}>When does it cost to be a part of JACT?</span>
+            When we get a request information or if we tip a club of you and a deal is on the way, you decides if you want to go further, with or without us.
+          A transfer is like genuin and taylormade from case to case, most of the the the client we represent pays, but in some case the club takes the economics in the transfer. Depending on the amount of time it takes to make a done deal, the fee is reglated, between 6000 sek and 9000 sek.
+
+          </div>
+          <div className={classes.qAndAText}>
+            <span className={classes.paragraph}>Why should I be in the playerbank?</span>
+            The floorballscene is growing, fast, and we see a big developement in the “big 4” countries regarding new players crossing the different boarders and leagues.
+Just becuse you are in the bank doesn´t  mean you have too change your situation, it only gives you alternative and opportunities to try something new.
+
           </div>
         </div>
       );
